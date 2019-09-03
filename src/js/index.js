82b8bb8e1da22d3fe40bbfd9dfa578cea84c384d
Patrick Godbey <mrpatg@gmail.com>
2019-09-02 22:58:17 -0400

    additiona fixes and rebuild

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,44 @@
-import 'bootstrap';
+import "bootstrap";
 
-import '../scss/index.scss';
+import "../scss/index.scss";
 
-const api_key = 'ba8159941b16e12958a2898e29ca82b1';
-const resultsContainer = $('#results-container');
-const totalResults = $('#totalresults');
-const castContainer = $('#castcontainer');
-const peoplePerson = $('#peopleperson');
-const imagebase = 'https://image.tmdb.org/t/p/w200/';
+const api_key = "ba8159941b16e12958a2898e29ca82b1";
+const resultsContainer = $("#results-container");
+const totalResults = $("#totalresults");
+const castContainer = $("#castcontainer");
+const peoplePerson = $("#peopleperson");
+const imagebase = "https://image.tmdb.org/t/p/w200/";
 
+const containers = [
+    resultsContainer,
+    totalResults,
+    castContainer,
+    peoplePerson
+];
 
-const containers = [resultsContainer, totalResults, castContainer, peoplePerson];
-
-$(document).ready(function ($) {
-    $('#search').keyup(function () {
-        let query = $('#search').val();
+$(document).ready(function($) {
+    $("#search").keyup(function() {
+        let query = $("#search").val();
         if (query.length > 3) {
             $.get(
-                'https://api.themoviedb.org/3/search/movie?api_key=' +
-                api_key +
-                '&language=en-US&query=' +
-                query +
-                '&page=1&include_adult=false',
-                function (data, status) {
+                "https://api.themoviedb.org/3/search/movie?api_key=" +
+                    api_key +
+                    "&language=en-US&query=" +
+                    query +
+                    "&page=1&include_adult=false",
+                function(data, status) {
                     console.log(data);
                     console.log(status);
                     searchResults(data);
                 }
             );
         }
-        if( !$('#search').val() ) {
-            $('.totalresults-container').removeClass('d-flex');
-            $('.totalresults-container').addClass('d-none');
-            resultsContainer.html('');
+        if (!$("#search").val()) {
+            $(".totalresults-container").removeClass("d-flex");
+            $(".totalresults-container").addClass("d-none");
+            resultsContainer.html("");
             resetElements(containers);
-      }
+        }
     });
 });
 
@@ -44,50 +48,52 @@ function searchResults(data) {
     let num_results = data.total_results;
 
     console.log(data.results);
-    $.each(data.results, function (index, value) {
+    $.each(data.results, function(index, value) {
         resultsContainer.append(
             '<li class="list-group-item movie-title-container d-flex justify-content-between lh-condensed"><span class="movie-title" data-movieid="' +
-            value.id +
-            '">' +
-            value.title +
-            '</span></li>'
+                value.id +
+                '">' +
+                value.title +
+                "</span></li>"
         );
     });
-    $('.totalresults-container').removeClass('d-none');
-    $('.totalresults-container').addClass('d-flex');
+    $(".totalresults-container").removeClass("d-none");
+    $(".totalresults-container").addClass("d-flex");
     totalResults.append(num_results);
-    $('.movie-title').on('click', function () {
-        $('.movie-title-container').removeClass('movie-list-selected');
-        castContainer.html('');
-        peoplePerson.html('');
-        $(this).parent('li').addClass('movie-list-selected');
-        getCredits($(this).data('movieid'));
+    $(".movie-title").on("click", function() {
+        $(".movie-title-container").removeClass("movie-list-selected");
+        castContainer.html("");
+        peoplePerson.html("");
+        $(this)
+            .parent("li")
+            .addClass("movie-list-selected");
+        getCredits($(this).data("movieid"));
     });
 }
 
 function getCredits(movieid) {
     $.get(
-        'https://api.themoviedb.org/3/movie/' +
-        movieid +
-        '/credits?api_key=' +
-        api_key +
-        '',
-        function (moviedata, status) {
+        "https://api.themoviedb.org/3/movie/" +
+            movieid +
+            "/credits?api_key=" +
+            api_key +
+            "",
+        function(moviedata, status) {
             console.log(moviedata.cast);
             console.log(status);
-            $.each(moviedata.cast, function (index, castvalue) {
+            $.each(moviedata.cast, function(index, castvalue) {
                 castContainer.append(
                     '<li class="list-group-item movie-title-container d-flex justify-content-between lh-condensed"><span class="person" data-personid="' +
-                    castvalue.id +
-                    '">' +
-                    castvalue.character +
-                    ' - ' +
-                    castvalue.name +
-                    '</span></li>'
+                        castvalue.id +
+                        '">' +
+                        castvalue.character +
+                        " - " +
+                        castvalue.name +
+                        "</span></li>"
                 );
             });
-            $('.person').on('click', function () {
-                getPerson($(this).data('personid'));
+            $(".person").on("click", function() {
+                getPerson($(this).data("personid"));
             });
         }
     );
@@ -95,33 +101,47 @@ function getCredits(movieid) {
 
 function getPerson(personid) {
     $.get(
-        'https://api.themoviedb.org/3/person/' +
-        personid +
-        '?api_key=' +
-        api_key +
-        '',
-        function (persondata, status) {
-            peoplePerson.html('');
+        "https://api.themoviedb.org/3/person/" +
+            personid +
+            "?api_key=" +
+            api_key +
+            "",
+        function(persondata, status) {
+            peoplePerson.html("");
             console.log(persondata);
             console.log(status);
-            peoplePerson.append('<h5>' + persondata.name + '</h5>');
-            peoplePerson.append('<span class="image"><img src="' + imagebase + persondata.profile_path + '"></span>');
-            if(persondata.place_of_birth){
-                peoplePerson.append('<span class="muted d-block">from ' + persondata.place_of_birth + '</span>');  
+            peoplePerson.append("<h5>" + persondata.name + "</h5>");
+            peoplePerson.append(
+                '<span class="image"><img src="' +
+                    imagebase +
+                    persondata.profile_path +
+                    '"></span>'
+            );
+            if (persondata.place_of_birth) {
+                peoplePerson.append(
+                    '<span class="muted d-block">from ' +
+                        persondata.place_of_birth +
+                        "</span>"
+                );
             }
-            peoplePerson.append('<span class="born-date d-block">Born: ' + persondata.birthday + ' </span>');
-            if(persondata.deathday){
-                peoplePerson.append('<span class="dead-date d-block">Died: ' + persondata.deathday + ' </span>');
-            }       
-            
-            
-
+            peoplePerson.append(
+                '<span class="born-date d-block">Born: ' +
+                    persondata.birthday +
+                    " </span>"
+            );
+            if (persondata.deathday) {
+                peoplePerson.append(
+                    '<span class="dead-date d-block">Died: ' +
+                        persondata.deathday +
+                        " </span>"
+                );
+            }
         }
     );
 }
 
 function resetElements(elements) {
     elements.forEach(element => {
-        element.html('');
+        element.html("");
     });
 }
